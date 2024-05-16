45461c48f2394cbfb11eeb128008497b0cd85500
Kimgeovedii <142533755+Kimgeovedii@users.noreply.github.com>
2024-05-16 22:41:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function redirectToQuestionnaire() {
+    // Ganti URL sesuai dengan halaman kuesioner Kualitas Layanan
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScfOFNhx49vNgYNKiZIIINSpDz0a7pUIHE3QfbzrIcyxwViiQ/viewform?usp=sf_link';
+}
+
+function redirectToInstructions() {
+    // Ganti URL sesuai dengan halaman petunjuk pengisian
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSdL-fZAM48o3IetRY4NnKOflimoK4tiv5CBfmGih8cTv-lIow/viewform';
+}
+
+function redirectToContact() {
+    // Ganti URL sesuai dengan halaman kontak
+    window.location.href = 'https://contoh.com/kontak';
+}
